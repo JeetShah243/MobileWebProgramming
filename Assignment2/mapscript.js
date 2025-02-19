758c1479614c446dba1047990c2e473b25358f53
JeetShah243 <jeetshah317@gmail.com>
2025-02-18 20:52:21 -0500

    store marker locations in an array

--- Assignment2/mapscript.js
+++ Assignment2/mapscript.js
@@ -8,3 +8,17 @@ function initMap()
      mapId: "MAP_ID_GOES_HERE"
   });
 }
+const locations = [
+    {name:"Dark Fox TCG", lat: 43.23749, lng: -79.88335, type:"TradingCard" },
+    {name:"Gamestop", lat: 43.22248, lng: -79.86138, type:"VideoGame" },
+    {name:"Never Board Games Lounge", lat: 43.25226, lng: -79.84558, type:"BoardGame" },
+    {name:"Top Games", lat: 43.23575, lng: -79.82773, type:"GameShop" },
+    {name:"Mancala Monk Board Game Cafe", lat: 43.25626, lng: -79.82018, type:"BoardGame" },
+    {name:"1UP Games", lat: 43.27076, lng: -79.89571, type:"VideoGame" },
+    {name:"Black Knight Games", lat: 43.22575, lng: -79.82979, type:"GameShop" },
+    {name:"Breakaway Sports Cards", lat: 43.23125, lng: -79.98772, type:"TradingCard" },
+    {name:"Super Castle Video", lat: 43.23575, lng: -79.73709, type:"VideoGame" },
+    {name:"The Bard and Bear Games Cafe", lat: 43.26938, lng: -79.86636, type:"BoardGame" },
+    {name:"456 Sports Cards and Memorabilia", lat: 43.26288, lng: -79.987425, type:"TradingCard" }
+];
+function getLocation(){}
